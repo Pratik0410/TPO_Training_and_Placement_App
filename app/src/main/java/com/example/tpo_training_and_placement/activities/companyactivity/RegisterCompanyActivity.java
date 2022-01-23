577d7bcac9c785b8c32d0d46f51c7c1d2a5c32e8
Prathamesh Sonawane <0410pratik@gmail.com>
2022-01-23 13:13:40 +0530

    RegisterCompanyActivity.java and activity_register_company.xml file modified.

--- app/src/main/java/com/example/tpo_training_and_placement/activities/companyactivity/RegisterCompanyActivity.java
+++ app/src/main/java/com/example/tpo_training_and_placement/activities/companyactivity/RegisterCompanyActivity.java
@@ -9,27 +9,30 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.example.tpo_training_and_placement.R;
 import com.example.tpo_training_and_placement.ui.CompaniesUi;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterCompanyActivity extends AppCompatActivity {
 
     public Button registerButton;
     public String[] productServiceArray = {"Product Based","Service Based","Both(Product & Service)"};
-    public EditText productServiceEditText;
+    public TextInputEditText companyNameTextInputEditText, productServiceTextInputEditText, aboutTextInputEditText, contactDetailsTextInputEditText;
     public TextInputLayout textInputLayout;
-    public AutoCompleteTextView autoCompleteTextView;
+    public AutoCompleteTextView typeOfCompanyAutoCompleteTextView;
 
     @Override
     protected void onResume() {
         super.onResume();
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(RegisterCompanyActivity.this,R.layout.dropdownlist_item,productServiceArray);
-        autoCompleteTextView.setAdapter(arrayAdapter);
+        typeOfCompanyAutoCompleteTextView.setAdapter(arrayAdapter);
     }
 
     @Override
@@ -41,40 +44,30 @@ public class RegisterCompanyActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
         registerButton = findViewById(R.id.id_register_company_button);
-        productServiceEditText = findViewById(R.id.id_product_service_edit_text);
+        companyNameTextInputEditText = findViewById(R.id.id_company_name_edit_text_in_register_company);
+        productServiceTextInputEditText = findViewById(R.id.id_product_service_edit_text);
+        aboutTextInputEditText = findViewById(R.id.id_about_edit_text_in_register_company);
+        contactDetailsTextInputEditText = findViewById(R.id.id_contact_details_edit_text_in_register_company);
         textInputLayout = findViewById(R.id.id_text_input_layout);
-        autoCompleteTextView = findViewById(R.id.id_auto_complete_textview);
+        typeOfCompanyAutoCompleteTextView = findViewById(R.id.id_auto_complete_textview);
 
-        autoCompleteTextView.setAdapter(arrayAdapter);
-        autoCompleteTextView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(productServiceArray[i] == "Product Based"){
-                    productServiceEditText.setHint("Name of Product");
-                }
-                else if(productServiceArray[i] == "Service Based"){
-                    productServiceEditText.setHint("Name of Service Provided");
-                }
-                else if(productServiceArray[i] == "Type of Company"){
-                    productServiceEditText.setHint("Select the Type of Company");
-                }
-                else if(productServiceArray[i] == "Both"){
-                    productServiceEditText.setHint("Name of Product and Service Provided");
-                }
-            }
+        typeOfCompanyAutoCompleteTextView.setAdapter(arrayAdapter);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+        registerButton.setOnClickListener(view -> {
 
-            }
-        });
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            DatabaseReference databaseReference = firebaseDatabase.getReference("List of Companies");
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegisterCompanyActivity.this, CompaniesUi.class));
-                finish();
-            }
+            Map map = new HashMap();
+            map.put("Company Name",companyNameTextInputEditText.getText().toString());
+            map.put("Type of Company",typeOfCompanyAutoCompleteTextView.getText().toString());
+            map.put("Product or Service of Company",productServiceTextInputEditText.getText().toString());
+            map.put("About Company",aboutTextInputEditText.getText().toString());
+            map.put("Contact Details",contactDetailsTextInputEditText.getText().toString());
+            databaseReference.child(companyNameTextInputEditText.getText().toString()).setValue(map);
+
+            startActivity(new Intent(RegisterCompanyActivity.this, CompaniesUi.class));
+            finish();
         });
 
 
