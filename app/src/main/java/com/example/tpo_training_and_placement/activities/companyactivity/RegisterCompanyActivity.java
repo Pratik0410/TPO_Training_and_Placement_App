bda5f5e81f5f1765b1e6bda93933437106e85510
Your Name <pranavmali105@gmail.com>
2022-01-25 13:08:34 +0530

    RegisterCompanyActivity.java Modified. CompaniesUi.java modified. RegisteredCompanyAdapter and RegisteredCompanyModel class added.

--- app/src/main/java/com/example/tpo_training_and_placement/activities/companyactivity/RegisterCompanyActivity.java
+++ app/src/main/java/com/example/tpo_training_and_placement/activities/companyactivity/RegisterCompanyActivity.java
@@ -1,22 +1,45 @@
 package com.example.tpo_training_and_placement.activities.companyactivity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.tpo_training_and_placement.R;
+import com.example.tpo_training_and_placement.adapters.RegisteredCompanyAdapter;
 import com.example.tpo_training_and_placement.ui.CompaniesUi;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,6 +50,12 @@ public class RegisterCompanyActivity extends AppCompatActivity {
     public TextInputEditText companyNameTextInputEditText, productServiceTextInputEditText, aboutTextInputEditText, contactDetailsTextInputEditText;
     public TextInputLayout textInputLayout;
     public AutoCompleteTextView typeOfCompanyAutoCompleteTextView;
+    public ImageView companyimageView;
+    public Uri filepath;
+    public Bitmap bitmap;
+    public int flagToChkUploadingImage=0;
+    public int productservice;
+
 
     @Override
     protected void onResume() {
@@ -50,26 +79,126 @@ public class RegisterCompanyActivity extends AppCompatActivity {
         contactDetailsTextInputEditText = findViewById(R.id.id_contact_details_edit_text_in_register_company);
         textInputLayout = findViewById(R.id.id_text_input_layout);
         typeOfCompanyAutoCompleteTextView = findViewById(R.id.id_auto_complete_textview);
+        companyimageView = findViewById(R.id.id_upload_company_logo_imageview);
 
         typeOfCompanyAutoCompleteTextView.setAdapter(arrayAdapter);
 
-        registerButton.setOnClickListener(view -> {
+        typeOfCompanyAutoCompleteTextView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                productservice = position;
+            }
 
-            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-            DatabaseReference databaseReference = firebaseDatabase.getReference("List of Companies");
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-            Map map = new HashMap();
-            map.put("Company Name",companyNameTextInputEditText.getText().toString());
-            map.put("Type of Company",typeOfCompanyAutoCompleteTextView.getText().toString());
-            map.put("Product or Service of Company",productServiceTextInputEditText.getText().toString());
-            map.put("About Company",aboutTextInputEditText.getText().toString());
-            map.put("Contact Details",contactDetailsTextInputEditText.getText().toString());
-            databaseReference.child(companyNameTextInputEditText.getText().toString()).setValue(map);
+            }
+        });
 
-            startActivity(new Intent(RegisterCompanyActivity.this, CompaniesUi.class));
-            finish();
+        companyimageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withActivity(RegisterCompanyActivity.this)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse response)
+                            {
+                                Intent intent=new Intent(Intent.ACTION_PICK);
+                                intent.setType("image/*");
+                                startActivityForResult(Intent.createChooser(intent,"Please select Image"),1);
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+
+                        }).check();
+
+            }
         });
 
 
+
+        registerButton.setOnClickListener(view -> {
+
+
+            if (companyNameTextInputEditText.getText().toString().length() !=0 && typeOfCompanyAutoCompleteTextView.getText().toString().length() != 0 && productServiceTextInputEditText.getText().toString().length() !=0
+            && aboutTextInputEditText.getText().toString().length() !=0 && contactDetailsTextInputEditText.getText().toString().length() !=0) {
+
+                if (flagToChkUploadingImage == 5) {
+                    FirebaseStorage storage = FirebaseStorage.getInstance();//image1 is your image name
+                    StorageReference uploader = storage.getReference().child(companyNameTextInputEditText.getText().toString());
+
+                    uploader.putFile(filepath)
+                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    Task<Uri> result = taskSnapshot.getStorage().getDownloadUrl();
+                                    result.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+                                            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                                            DatabaseReference databaseReference = firebaseDatabase.getReference("List of Companies");
+
+                                            Map map = new HashMap();
+                                            map.put("CompanyName", companyNameTextInputEditText.getText().toString());
+                                            map.put("Type of Company", typeOfCompanyAutoCompleteTextView.getText().toString());
+                                            map.put("Product or Service of Company", productServiceTextInputEditText.getText().toString());
+                                            map.put("About Company", aboutTextInputEditText.getText().toString());
+                                            map.put("Contact Details", contactDetailsTextInputEditText.getText().toString());
+                                            map.put("Company Logo", uri.toString());
+                                            databaseReference.child(companyNameTextInputEditText.getText().toString()).setValue(map);
+                                        }
+                                    });
+                                }
+                            });
+
+
+                    startActivity(new Intent(RegisterCompanyActivity.this, CompaniesUi.class));
+                    finish();
+                } else {
+                    Toast.makeText(RegisterCompanyActivity.this, "Please Select Image", Toast.LENGTH_SHORT).show();
+                }
+            } else{
+                Toast.makeText(RegisterCompanyActivity.this, "Please Fill All Details", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        if(requestCode==1 && resultCode==RESULT_OK)
+        {
+            filepath=data.getData();//filepath is nothing but a selected image url
+            try
+            {
+                InputStream inputStream=getContentResolver().openInputStream(filepath);
+                bitmap= BitmapFactory.decodeStream(inputStream);
+                companyimageView.setImageBitmap(bitmap);
+                flagToChkUploadingImage=5;
+
+            }catch (Exception ex)
+            {
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(RegisterCompanyActivity.this, CompaniesUi.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/tpo_training_and_placement/ui/CompaniesUi.java
+++ app/src/main/java/com/example/tpo_training_and_placement/ui/CompaniesUi.java
@@ -2,6 +2,8 @@ package com.example.tpo_training_and_placement.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,11 +12,19 @@ import android.widget.ImageButton;
 
 import com.example.tpo_training_and_placement.R;
 import com.example.tpo_training_and_placement.activities.companyactivity.RegisterCompanyActivity;
+import com.example.tpo_training_and_placement.adapters.RegisteredCompanyAdapter;
+import com.example.tpo_training_and_placement.models.PrePlacementModel;
+import com.example.tpo_training_and_placement.models.RegisteredCompanyModel;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CompaniesUi extends AppCompatActivity {
 
     public CardView companiesUiCardView;
     public ImageButton registerCompanyImageButton;
+    public RecyclerView registeredcompanyRecyclerView;
+    public RegisteredCompanyAdapter registeredCompanyAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,19 @@ public class CompaniesUi extends AppCompatActivity {
 
         companiesUiCardView = findViewById(R.id.id_ui_pre_placement);
         registerCompanyImageButton = findViewById(R.id.id_register_company);
+        registeredcompanyRecyclerView = findViewById(R.id.id_list_of_companies_recycler_view);
+
+        registeredcompanyRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<RegisteredCompanyModel> options =
+                new FirebaseRecyclerOptions.Builder<RegisteredCompanyModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("List of Companies"), RegisteredCompanyModel.class)
+                        .build();
+
+        registeredCompanyAdapter = new RegisteredCompanyAdapter(options);
+        registeredcompanyRecyclerView.setAdapter(registeredCompanyAdapter);
+
+
 
         companiesUiCardView.setBackgroundResource(R.drawable.top_card_radius);
 
@@ -32,8 +55,22 @@ public class CompaniesUi extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(CompaniesUi.this, RegisterCompanyActivity.class));
+                finish();
             }
         });
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registeredCompanyAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        registeredCompanyAdapter.stopListening();
     }
 }
