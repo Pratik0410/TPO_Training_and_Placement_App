534df59d93bc28de02d9cf04f3c7dad903cbc59c
Prathamesh Sonawane <0410pratik@gmail.com>
2022-01-27 19:19:31 +0530

    PlacementHistoryUi.java and ui_placement_opportunity.xml File modified.

--- app/src/main/java/com/example/tpo_training_and_placement/ui/PlacementOpportunityUi.java
+++ app/src/main/java/com/example/tpo_training_and_placement/ui/PlacementOpportunityUi.java
@@ -5,36 +5,38 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 
 import com.example.tpo_training_and_placement.R;
-import com.example.tpo_training_and_placement.activities.currentcompanyactivity.RegisterCurrentCompanyActivity;
-import com.example.tpo_training_and_placement.activities.placementopportunity.JobOpportunityActivity;
+import com.example.tpo_training_and_placement.data.EditPlacementData;
+import com.example.tpo_training_and_placement.data.SelectPlacementCompanyData;
+
+import java.util.Objects;
 
 public class PlacementOpportunityUi extends AppCompatActivity {
 
     public CardView placementOpportunityUiCardView;
-    public ImageButton jobOpportunityImageButton;
+    public CardView editPlacementOpportunitiesCardView;
+    public CardView addPlacementOpportunitiesCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ui_placement_opportunity);
 
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         placementOpportunityUiCardView = findViewById(R.id.id_ui_placement_opportunity);
-        jobOpportunityImageButton = findViewById(R.id.id_job_opportunities);
+        editPlacementOpportunitiesCardView = findViewById(R.id.id_edit_placement_opportunities);
+        addPlacementOpportunitiesCardView = findViewById(R.id.id_add_placement_opportunities);
 
         placementOpportunityUiCardView.setBackgroundResource(R.drawable.top_card_radius);
 
-        jobOpportunityImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(PlacementOpportunityUi.this, JobOpportunityActivity.class));
-                finish();
-            }
+        editPlacementOpportunitiesCardView.setOnClickListener(view -> {
+            startActivity(new Intent(PlacementOpportunityUi.this, EditPlacementData.class));
+        });
+
+        addPlacementOpportunitiesCardView.setOnClickListener(view -> {
+            startActivity(new Intent(PlacementOpportunityUi.this, SelectPlacementCompanyData.class));
         });
 
     }
