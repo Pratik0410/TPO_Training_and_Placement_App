cb0a0e80d4a5a5864ac3e1e05240f95ff1259b35
Prathamesh Sonawane <0410pratik@gmail.com>
2022-01-28 15:50:01 +0530

    Pre-Placement Talks Ui Modified.

--- app/src/main/java/com/example/tpo_training_and_placement/data/PreviousPrePlacementTalksData.java
+++ app/src/main/java/com/example/tpo_training_and_placement/data/PreviousPrePlacementTalksData.java
@@ -1,32 +1,34 @@
-package com.example.tpo_training_and_placement.activities.preplacementactivity;
+package com.example.tpo_training_and_placement.data;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.example.tpo_training_and_placement.R;
 import com.example.tpo_training_and_placement.adapters.PrePlacementAdapter;
 import com.example.tpo_training_and_placement.models.PrePlacementModel;
-import com.example.tpo_training_and_placement.models.SentNotificationModel;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
-public class PreviousPrePlacementTalksActivity extends AppCompatActivity {
+public class PreviousPrePlacementTalksData extends AppCompatActivity {
 
+    public ImageButton arrowBackImageButton;
     RecyclerView previousPrePlacementRecyclerView;
     PrePlacementAdapter prePlacementAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_previous_pre_placement_talks);
+        setContentView(R.layout.data_previous_pre_placement_talks);
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        arrowBackImageButton = findViewById(R.id.id_arrow_back_image_button_in_data_previous_pre_placement_talk);
         previousPrePlacementRecyclerView = findViewById(R.id.id_recycler_view_in_previous_pre_placement);
         previousPrePlacementRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -38,6 +40,7 @@ public class PreviousPrePlacementTalksActivity extends AppCompatActivity {
         prePlacementAdapter = new PrePlacementAdapter(options);
         previousPrePlacementRecyclerView.setAdapter(prePlacementAdapter);
 
+        arrowBackImageButton.setOnClickListener(view -> finish());
     }
 
     @Override
