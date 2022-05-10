f758f72d71172b7c2c8413b8a96eff1b0f81d806
Nishigandha Ingle <nishigandhaingle2403@gmail.com>
2022-05-10 15:51:22 +0530

    data_view_company_placement_history.xml, ic_baseline_expand_less_24.xml, ic_baseline_expand_more_24.xml, model_placement_history_card.xml, model_placement_history_company_card.xml and ViewCompanyPlacementHistoryData.java modified

---
+++ app/src/main/java/com/example/tpo_training_and_placement/data/ViewCompanyPlacementHistoryData.java
@@ -0,0 +1,14 @@
+package com.example.tpo_training_and_placement.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewCompanyPlacementHistoryData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_view_company_placement_history);
+    }
+}
